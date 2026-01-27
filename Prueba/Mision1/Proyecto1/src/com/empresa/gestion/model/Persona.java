19a99e104a6807e2f6f062be171dbdfae6a59362
Susana Acosta <vivianacosta321450@correo.itm.edu.co>
2026-01-27 17:13:48 -0500

    refactorizacion de codigo

--- Prueba/Mision1/Proyecto1/src/com/empresa/gestion/model/Persona.java
+++ Prueba/Mision1/Proyecto1/src/com/empresa/gestion/model/Persona.java
@@ -1,29 +1,47 @@
-package Mision1.Proyecto1.src.com.empresa.gestion.model;
+package com.empresa.gestion.model;
 
 public class Persona {
-    
-    //Encapsulacion
+    //🔐 Encapsulación
     private String nombre;
-    private int edad;
-    //Constructor
-    public Persona(String nombre, int edad) {
+    protected int edad;
+    protected boolean activo;// true false
+    protected char genero;// 'M'   o 'F'
+    //🧱 Constructor
+    public Persona(String nombre, int edad,boolean activo,char genero) {
         this.nombre = nombre;
         this.edad = edad;
-        
-
-        }
-
-        public String getNombre() {
-            return nombre;
-
-        } public void SetNombre(String nombre) {
-            this.nombre = nombre;
-
-        } 
-        //metodos
-        public void presentarse(){
-            system.out.println("hola, soy"+nombre+" y tengo "+edad+" años");
-        }
-
+        this.activo= activo;
+        this.genero=genero;
     }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public char getGenero() {
+        return genero;
+    }
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+    
+//Método
+    public void presentarse(){
+        System.out.println("Hola, soy"+nombre+" y tengo "+edad+" años");
+    }
+    public void mostrarInfoBasica(){
+        System.out.println("Nombre: "+ nombre);
+        System.out.println("Edad: "+ edad);
+        System.out.println("Activo: " + activo);
+        System.out.println("Género: "+ genero);
 
+         
+    }
+}
